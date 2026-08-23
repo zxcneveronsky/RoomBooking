b8d23827ad7cfcdb5e9f09f9148ac2c5fc7c41de
vears <stafeewgrisha@gmail.com>
2026-08-23 18:49:51 +0500

    дебаг букинг ентити

--- src/main/java/com/example/roombooking/entity/BookingEntity.java
+++ src/main/java/com/example/roombooking/entity/BookingEntity.java
@@ -1,28 +1,38 @@
 package com.example.roombooking.entity;
 
-import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
-@Table(name="Bookings")
+@Table(name = "bookings")
+@Getter
+@Setter
 public class BookingEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
 
     @ManyToOne
-    @JoinColumn(name="room_id")
+    @JoinColumn(name = "room_id", nullable = false)
     private RoomEntity room;
 
-    @Column(name="start_at",nullable = false)
+    @Column(name = "start_at", nullable = false)
     private LocalDateTime startAt;
 
-    @Column(name="start_at",nullable = false)
+    @Column(name = "end_at", nullable = false)
     private LocalDateTime endAt;
-
 }

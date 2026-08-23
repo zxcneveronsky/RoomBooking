ea2f528200d2f67bfac8a6f7ea3a9b14c2b8b17b
vears <stafeewgrisha@gmail.com>
2026-08-23 21:56:21 +0500

    обновил ограничения в дто

--- src/main/java/com/example/roombooking/dto/request/CreateBookingRequest.java
+++ src/main/java/com/example/roombooking/dto/request/CreateBookingRequest.java
@@ -1,12 +1,11 @@
 package com.example.roombooking.dto.request;
 
 import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
-public record BookingRequest(
+public record CreateBookingRequest(
         @NotNull(message="ID комнаты не может быть пустым")
         Long roomId,
 
--- src/main/java/com/example/roombooking/dto/request/UpdateBookingRequest.java
+++ src/main/java/com/example/roombooking/dto/request/UpdateBookingRequest.java
@@ -1,5 +1,6 @@
 package com.example.roombooking.dto.request;
 
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
@@ -7,7 +8,10 @@ import java.time.LocalDateTime;
 public record UpdateBookingRequest(
         @NotNull(message="ID брони не может быть пустым")
         Long id,
+        @NotNull(message="ID комнаты не может быть пустым")
         Long roomId,
+        @FutureOrPresent(message="Дата начала не может быть в прошлом")
         LocalDateTime startAt,
+        @FutureOrPresent(message="Дата начала не может быть в прошлом")
         LocalDateTime endAt
 ){}

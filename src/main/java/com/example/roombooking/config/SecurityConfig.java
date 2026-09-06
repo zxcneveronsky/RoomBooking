e9b73d8e4b1b9153bcde2a9f11c7bcb0ca24c049
Shtefan <stepakropocev@gmail.com>
2026-09-06 13:21:25 +0500

    добавил swagger

--- src/main/java/com/example/roombooking/config/SecurityConfig.java
+++ src/main/java/com/example/roombooking/config/SecurityConfig.java
@@ -40,6 +40,8 @@ public class SecurityConfig {
                         "/",
                         "/*.html",
                         "/js/**",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
                         "/api/v1/auth/login",
                         "/api/v1/auth/register"
                 ).permitAll()
